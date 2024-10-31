6a402b289846c08386d65ccb2cf8632145ac0253
Ahmad <rabieahmad330@gmail.com>
2024-10-31 17:56:17 +0300

    almost finished working on the cards

--- my-shop/src/Components/Category/Category.jsx
+++ my-shop/src/Components/Category/Category.jsx
@@ -9,16 +9,43 @@ const Category = () => {
     <div className="py-8">
       <div className="container">
         <div className="grid grid-cols-1 sm:placeholder:grid-cols-2 lg:grid-cols-4 gap-8">
-            <div className="py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl">
-                <div>
-                    <div>
-                        <p>Enjoy</p>
-                        <p>With</p>
-                        <p>Earphone</p>
-                        <Button text="Browse" textColor={"text-white"} bgColor={"bg-primary"}/>
-                    </div>
-                </div>
+          {/*first col */}
+          <div className="py-10 pl-5 bg-gradient-to-br from-black/90 to-brandYellow/70 text-white rounded-3xl relative h-[320px] flex items-end">
+            <div>
+              <div className="mb-4">
+                <p className="mb-[2px] text-gray-400">Enjoy</p>
+                <p className="text-2xl font-semibold mb[2px]">With</p>
+                <p className="text-4xl xl:text-5xl font-bold opacity-20 mb-2">
+                  Earphone
+                </p>
+                <Button
+                  text="Browse"
+                  textColor={"text-white"}
+                  bgColor={"bg-primary"}
+                />
+              </div>
             </div>
+            <img src={Image1} alt="" className="w-[320px] absolute bottom-0" />
+          </div>
+          {/*second col */}
+          <div className="py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end">
+            <div>
+              <div className="mb-4">
+                <p className="mb-[2px] text-gray-400">Enjoy</p>
+                <p className="text-2xl font-semibold mb[2px]">With</p>
+                <p className="text-4xl xl:text-5xl font-bold opacity-20 mb-2">
+                  Earphone
+                </p>
+                <Button
+                  text="Browse"
+                  textColor={"text-white"}
+                  bgColor={"bg-primary"}
+                />
+              </div>
+            </div>
+            <img src={Image2} alt="" className="w-[320px] absolute -right-4 lg:top-[40px]" />
+          </div>
+          {/*third col */}
         </div>
       </div>
     </div>
